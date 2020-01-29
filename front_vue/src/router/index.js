1adf0b3e02d5c0453fecb6b4de5a65c103cd246b
jbh <ceajer@gmail.com>
2020-01-29 17:54:27 +0900

    login form | data not yet

--- front_vue/src/router/index.js
+++ front_vue/src/router/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Login from '../views/Login.vue'
 
 Vue.use(VueRouter)
-
-const routes = [
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
   {
     path: '/',
     name: 'home',
@@ -13,15 +16,13 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: About
+  },
+  {
+    path:'/login',
+    name: 'login',
+    component: Login
+  },
 ]
-
-const router = new VueRouter({
-  routes
 })
-
 export default router
