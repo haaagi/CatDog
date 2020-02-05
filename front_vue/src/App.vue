bf8a695992f210118de6148dc2e4b5a31a008bab
jbh <ceajer@gmail.com>
2020-02-05 17:11:42 +0900

    want pull

--- front_vue/src/App.vue
+++ front_vue/src/App.vue
@@ -75,25 +75,27 @@
 
       <v-content style="height:2000px"> <router-view /> </v-content>
       <v-content>
-        <v-footer absolute color="amber" class="white--text">
-          <v-card flat tile class="amber lighten-1 white--text text-center">
-            <v-card-text class="white--text pt-0">
-              Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus
-              commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed
-              at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et
-              faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices,
-              cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut
-              orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-              ridiculus mus.
-            </v-card-text>
+        <div v-if="isLoggedIn">
+          <v-footer absolute color="amber" class="white--text">
+            <v-card flat tile class="amber lighten-1 white--text text-center">
+              <v-card-text class="white--text pt-0">
+                Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus
+                commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed
+                at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et
+                faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices,
+                cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel
+                ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+                ridiculus mus.
+              </v-card-text>
 
-            <v-divider />
+              <v-divider />
 
-            <v-card-text class="white--text">
-              <strong>Vuetify</strong>
-            </v-card-text>
-          </v-card>
-        </v-footer>
+              <v-card-text class="white--text">
+                <strong>Vuetify</strong>
+              </v-card-text>
+            </v-card>
+          </v-footer>
+        </div>
       </v-content>
     </div>
   </v-app>
