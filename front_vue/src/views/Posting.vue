14355265da0cb7cf8dc9efe25b3eec6e9df970fc
jbh <ceajer@gmail.com>
2020-02-06 18:02:02 +0900

    posting page function

--- front_vue/src/views/Posting.vue
+++ front_vue/src/views/Posting.vue
@@ -1,55 +1,91 @@
 <template>
-  <div class="">
-    <div>
-      <div class="file-upload-form">
-        Upload an image file:
-        <input type="file" @change="previewImage" accept="image/*" />
-      </div>
-      <div class="image-preview" v-if="imageData.length > 0">
-        <img class="preview" :src="imageData" />
-      </div>
-    </div>
+  <div>
+    <file-pond
+      name="bin"
+      ref="pond"
+      allow-multiple="false"
+      max-files="1"
+      accepted-file-types="image/jpeg, image/png"
+      v-bind:files="myFiles"
+      v-on:init="handleFilePondInit"
+      v-on:processfile="onload"
+    />
   </div>
 </template>
 
 <script>
+const axios = require('axios');
+// Import Vue FilePond
+import vueFilePond from 'vue-filepond';
+
+// Import FilePond styles
+import 'filepond/dist/filepond.min.css';
+
+// Import FilePond plugins
+// Please note that you need to install these plugins separately
+
+// Import image preview plugin styles
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
+
+// Import image preview and file type validation plugins
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+
+// Create component
+const FilePond = vueFilePond(FilePondPluginFileValidateType, FilePondPluginImagePreview);
+
 export default {
-  data: () => ({
-    //
-    imageData: '',
-  }),
+  name: 'app',
+  data() {
+    return {
+      myFiles: [],
+      // server: {
+      //   url: `http://70.12.246.72:8080/api/user/profileimg/jbh@naver.com`,
+      //   process: {
+      //     method: 'POST',
+      //     headers: {
+      //       Authorization: 'JWT' + sessionStorage.getItem('jwt'),
+      //     },
+      //   },
+      // },
+    };
+  },
   methods: {
-    previewImage: function(event) {
-      // Reference to the DOM input element
-      var input = event.target;
-      // Ensure that you have a file before attempting to read it
-      if (input.files && input.files[0]) {
-        // create a new FileReader to read this image and convert to base64 format
-        var reader = new FileReader();
-        // Define a callback function to run, when FileReader finishes its job
-        reader.onload = e => {
-          // Note: arrow function used here, so that "this.imageData" refers to the imageData of Vue component
-          // Read image as base64 and set to imageData
-          this.imageData = e.target.result;
-        };
-        // Start the reader job - read file as a data url (base64 format)
-        reader.readAsDataURL(input.files[0]);
-      }
+    processHandler: (fieldName, file, metadata, load, error, progress) => {
+      // set data
+      const formData = new FormData();
+      formData.append('file', file, file.name);
+
+      // related to aborting the request
+      const CancelToken = axios.CancelToken;
+      const source = CancelToken.source();
+
+      // the request itself
+      axios({
+        method: 'post',
+        url: 'http://70.12.246.72:8080/api/user/profileimg/jbh@naver.com',
+        data: formData,
+        cancelToken: source.token,
+        onUploadProgress: e => {
+          // updating progress indicator
+          progress(e.lengthComputable, e.loaded, e.total);
+        },
+      })
+        .then(response => {
+          // passing the file id to FilePond
+          load(response.data.data.id);
+        })
+        .catch(thrown => {
+          if (axios.isCancel(thrown)) {
+            console.log('Request canceled', thrown.message);
+          } else {
+            // handle error
+          }
+        });
     },
+  },
+  components: {
+    FilePond,
   },
 };
 </script>
-
-<style scoped>
-.file-upload-form,
-.image-preview {
-  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-  padding: 20px;
-}
-img.preview {
-  width: 200px;
-  background-color: white;
-  border: 1px solid #ddd;
-  padding: 5px;
-}
-</style>
