701e2946e70c1f6998f37e26e9b8df0e3acc27bb
ponyong <ponyong@naver.com>
2020-02-10 14:52:25 +0900

    WebSocket을 통한 서버/클라이언트 통신 구현

---
+++ back/src/main/java/com/catdog/springboot/web/ChatController.java
@@ -0,0 +1,25 @@
+package com.catdog.springboot.web;
+
+import com.catdog.springboot.service.ChatService;
+import com.catdog.springboot.web.dto.ChatRoom;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+
+    private final ChatService chatService;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRoom() {
+        return chatService.findAllRoom();
+    }
+}
