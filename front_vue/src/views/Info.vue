339df958e506bebee10da2ddf624dd36d47adba9
ponyong <ponyong@naver.com>
2020-02-19 18:12:52 +0900

    detail fix

--- front_vue/src/views/Info.vue
+++ front_vue/src/views/Info.vue
@@ -5,27 +5,26 @@
       <div class="row row-bottom-padded-md">
         <div class="col-md-3 col-sm-6 col-padding animate-box" data-animate-effect="fadeInLeft">
           <div class="blog-entry">
-            <div class="blog-img"
-              ><img 
+            <div class="blog-img">
+              <img
                 style="height: 200px"
                 src="../assets/img/Pomeranian.jpg"
                 class="img-responsive"
                 alt="Free HTML5 Bootstrap Template by FreeHTML5.co"
-            /></div>
+              />
+            </div>
             <div class="desc">
               <h3>Pomeranian</h3>
-              <span
-                >수명 : 12~16년 / 무게 : 1.9~3.5kg</span
-              >
+              <span>수명 : 12~16년 / 무게 : 1.9~3.5kg</span>
               <p>
-                포메라니안은 중앙유럽에 있는 포메라니아 지역에서 유래된 스피츠 종류의 애완견이다. 
+                포메라니안은 중앙유럽에 있는 포메라니아 지역에서 유래된 스피츠 종류의 애완견이다.
               </p>
             </div>
           </div>
         </div>
         <div class="col-md-3 col-sm-6 col-padding animate-box" data-animate-effect="fadeInLeft">
           <div class="blog-entry">
-            <a  class="blog-img"
+            <a class="blog-img"
               ><img
                 style="height: 200px"
                 src="../assets/img/beagle.jpg"
@@ -34,11 +33,9 @@
             /></a>
             <div class="desc">
               <h3>Beagle</h3>
-              <span
-                >수명 : 12~16년 / 무게 : 8~14kg</span
-              >
+              <span>수명 : 12~16년 / 무게 : 8~14kg</span>
               <p>
-                외모는 억세어 보이며, 몸에 군더더기 살이 없고, 짧은 털이 빽빽하게 나 있다. 
+                외모는 억세어 보이며, 몸에 군더더기 살이 없고, 짧은 털이 빽빽하게 나 있다.
               </p>
             </div>
           </div>
@@ -54,9 +51,7 @@
             /></a>
             <div class="desc">
               <h3>Bulldog</h3>
-              <span
-                >수명 : 8~10년 / 무게 : 18~25kg</span
-              >
+              <span>수명 : 8~10년 / 무게 : 18~25kg</span>
               <p>
                 불도그는 영국 잉글랜드 원산의 개이며 잉글리쉬 불독이라고도 불린다.
               </p>
@@ -74,11 +69,10 @@
             /></a>
             <div class="desc">
               <h3>French-bulldog</h3>
-               <span
-                >수명 : 10~14년 / 무게 : 10~13kg</span
-              >
+              <span>수명 : 10~14년 / 무게 : 10~13kg</span>
               <p>
-                프렌치 불도그는 프랑스 원산으로 기원은 확실하지 않으나 1860년경 퍼그와 테리어의 교배로 생겨난 것으로 여겨진다.
+                프렌치 불도그는 프랑스 원산으로 기원은 확실하지 않으나 1860년경 퍼그와 테리어의
+                교배로 생겨난 것으로 여겨진다.
               </p>
             </div>
           </div>
@@ -95,11 +89,10 @@
             /></a>
             <div class="desc">
               <h3>Golden-retriever</h3>
-               <span
-                >수명 : 10~12년 / 무게 : 25~34kg</span
-              >
+              <span>수명 : 10~12년 / 무게 : 25~34kg</span>
               <p>
-                골든 리트리버는 개 품종으로 원산지는 영국 스코틀랜드이다. 근육질의 몸에 건강하고 산뜻한 느낌을 준다. 
+                골든 리트리버는 개 품종으로 원산지는 영국 스코틀랜드이다. 근육질의 몸에 건강하고
+                산뜻한 느낌을 준다.
               </p>
             </div>
           </div>
@@ -115,11 +108,10 @@
             /></a>
             <div class="desc">
               <h3>Toy Poodle</h3>
-               <span
-                >수명 : 12~15년 / 무게 : 2~3kg</span
-              >
+              <span>수명 : 12~15년 / 무게 : 2~3kg</span>
               <p>
-                푸들은 개의 품종 중 하나이며, 프랑스의 국견이다. 원산지는 독일이고, 프랑스 귀족 여성들에 의해 인기를 얻어 널리 번식되었으며, 털이 길고 양털 모양으로 자란다. 
+                푸들은 개의 품종 중 하나이며, 프랑스의 국견이다. 원산지는 독일이고, 프랑스 귀족
+                여성들에 의해 인기를 얻어 널리 번식되었으며, 털이 길고 양털 모양으로 자란다.
               </p>
             </div>
           </div>
@@ -135,10 +127,10 @@
             /></a>
             <div class="desc">
               <h3>Shepherd</h3>
-               <span
-                >수명 : 9~13년 / 무게 : 22~40kg</span>
+              <span>수명 : 9~13년 / 무게 : 22~40kg</span>
               <p>
-                저먼 셰퍼드는 원산지가 독일이다. 몸은 빈틈이 없는 근육질이고 꼬리는 늘어졌다. 앞뒷다리는 비교적 짧지만 튼튼하고 실팍하다. 
+                저먼 셰퍼드는 원산지가 독일이다. 몸은 빈틈이 없는 근육질이고 꼬리는 늘어졌다.
+                앞뒷다리는 비교적 짧지만 튼튼하고 실팍하다.
               </p>
             </div>
           </div>
@@ -154,8 +146,7 @@
             /></a>
             <div class="desc">
               <h3>Shorthairedpointer</h3>
-              <span
-                >수명 : 12~14년 / 무게 : 20~32kg</span>
+              <span>수명 : 12~14년 / 무게 : 20~32kg</span>
               <p>
                 독일 단발 포인터는 사냥을 위해 독일에서 19 세기에 개발 된 중대형 개 품종입니다.
               </p>
@@ -174,17 +165,17 @@
             /></a>
             <div class="desc">
               <h3>Yorkshire-terrier</h3>
-              <span
-                >수명 : 13~16년 / 무게 : 3~3.2kg</span>
+              <span>수명 : 13~16년 / 무게 : 3~3.2kg</span>
               <p>
-                요크셔 테리어는 영국의 개 품종 중의 하나이며, 한때 노동자들이 기르던 개이며, 쥐잡기용 강아지로 인기가 높았던 애완견이다. 
+                요크셔 테리어는 영국의 개 품종 중의 하나이며, 한때 노동자들이 기르던 개이며,
+                쥐잡기용 강아지로 인기가 높았던 애완견이다.
               </p>
             </div>
           </div>
         </div>
         <div class="col-md-3 col-sm-6 col-padding animate-box" data-animate-effect="fadeInLeft">
           <div class="blog-entry">
-            <a  class="blog-img"
+            <a class="blog-img"
               ><img
                 style="height: 200px"
                 src="../assets/img/Chihuahua.jpg"
@@ -193,8 +184,7 @@
             /></a>
             <div class="desc">
               <h3>Chihuahua</h3>
-              <span
-                >수명 : 12~20년 / 무게 : 1.5~3kg</span>
+              <span>수명 : 12~20년 / 무게 : 1.5~3kg</span>
               <p>
                 치와와는 멕시코의 개 품종 중의 하나이다. 멕시코 치와와주의 이름에서 왔다.
               </p>
@@ -203,26 +193,26 @@
         </div>
         <div class="col-md-3 col-sm-6 col-padding animate-box" data-animate-effect="fadeInLeft">
           <div class="blog-entry">
-            <a  class="blog-img"
+            <a class="blog-img"
               ><img
-               style="height: 200px"
+                style="height: 200px"
                 src="../assets/img/der Dachs.jpg"
                 class="img-responsive"
                 alt="Free HTML5 Bootstrap Template by FreeHTML5.co"
             /></a>
             <div class="desc">
               <h3>der Dachs</h3>
-              <span
-                >수명 : 12~16년 / 무게 : 7~15kg</span>
+              <span>수명 : 12~16년 / 무게 : 7~15kg</span>
               <p>
-                닥스훈트는 몸통이 길고 사지가 짧은 독일 원산의 개로서, 원래는 오소리 사냥의 전문개였다.
+                닥스훈트는 몸통이 길고 사지가 짧은 독일 원산의 개로서, 원래는 오소리 사냥의
+                전문개였다.
               </p>
             </div>
           </div>
         </div>
         <div class="col-md-3 col-sm-6 col-padding animate-box" data-animate-effect="fadeInLeft">
           <div class="blog-entry">
-            <a  class="blog-img"
+            <a class="blog-img"
               ><img
                 style="height: 200px"
                 src="../assets/img/Sichuan.jpg"
@@ -231,8 +221,7 @@
             /></a>
             <div class="desc">
               <h3>Sichuan</h3>
-              <span
-                >수명 : 10~16년 / 무게 : 4~7.2kg</span>
+              <span>수명 : 10~16년 / 무게 : 4~7.2kg</span>
               <p>
                 시추는 중국에서 비롯한 개 품종이며 과거 중국 황실이 기르던 견종이다.
               </p>
